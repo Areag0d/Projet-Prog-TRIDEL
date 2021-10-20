3f2465bc05f694c1e79ffe7b62200ed77d20651d
Mike <mimagon1@gmail.com>
2021-10-20 16:36:41 +0200

    some inital updates

--- Part1 TRIDEL.py
+++ Part1 TRIDEL.py
@@ -7,3 +7,32 @@ Created on Wed Oct 20 15:57:44 2021
 Part 1 of TRIDEL modelisation: data generation
 """
 
+import math
+import numpy as np
+
+"""on définit nos variables de départ"""
+
+#habitants par région:
+#on a les habitants totaux du canton de Vaud
+habTotal = 
+
+#on a le pourcentage que représentent les habitants
+#d'une région vis-à-vis du canton
+prophabLausanne =
+prophabNord =
+prophabOuest = 
+prophabLaCote =
+
+#On calcule simplement la quantité d'habitants par région
+#grâce aux données précédentes
+habLausanne  = habTotal * prophabLausanne
+habNord = habTotal *
+habOuest =
+habLaCote = 
+
+#déchets par habitant par région : wastehabRegion
+wastehabLausanne = 
+wastehabNord =
+wastehabOuest =  
+wastehabLaCote = 
+
