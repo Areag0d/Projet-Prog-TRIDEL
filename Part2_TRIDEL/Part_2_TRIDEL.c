3aa1de1888a0ecf59f603852241aa2b0719268f2
Mikael <mikael.gonzalez@epfl.ch>
2021-11-12 13:02:50 +0100

    Corrected Cm_Inert (hopefully last version)

--- Part2_TRIDEL/Part_2_TRIDEL.c
+++ Part2_TRIDEL/Part_2_TRIDEL.c
@@ -79,15 +79,12 @@ double OriginalMass(double massOxy, double MWOxy, double MWPure, double StoichCo
 }
 
 //This function takes in argument the massic proportions of the compostion of
-//machefer. It is assumed to be composed of SiO2, Al2O3, CaO, Fe2O3 and trace elements.
+//machefer. It is assumed to be composed of SiO2, Al2O3, CaO, Fe2O3, C and Cl.
 //Knowing relative proportions of components of the inert part of waste,
 //we can calculate its specific heat, which is essential for downstream calculus
-double Cm_Inert(double propSiO2, double propAl2O3, double propCaO, double propFe2O3){
-  //we group all trace elements under an only category: others.
-  //we obtain the proportion of trace elements with a substraction
-  double propOthers = 100 - (propSiO2 + propAl2O3 + propCaO + propFe2O3);
-
-  //Except for SiO2, which is glass, and trace elements, all the other components
+//We neglect the contribution of trace elements, as their proportion is insignificant.
+double Cm_Inert(double propSiO2, double propAl2O3, double propCaO, double propFe2O3, double propC, double propCl){
+  //Except for SiO2, which is glass, Carbon and Chlorine, all the other components
   //are in their oxidized form, which means there were burnt.
   //therefore to calculate their proportions in incoming waste,
   //we need to calculate their proportions before oxidation (metallic)
@@ -96,8 +93,11 @@ double Cm_Inert(double propSiO2, double propAl2O3, double propCaO, double propFe
   //Glass (SiO2)
   double mSiO2 = propSiO2 * 1; //[g]
 
-  //Others
-  double mOthers = propOthers + 1; //[g]
+  //Carbon
+  double mC = propC * 1; //[g]
+
+  //Chlorine
+  double mCl = propCl * 1; //[g]
 
   //Aluminium
   //Tabulated Molar Weight of Al2O3 = 102 [g/mol], Al = 27 [g/mol]
@@ -118,7 +118,7 @@ double Cm_Inert(double propSiO2, double propAl2O3, double propCaO, double propFe
   double mCa = OriginalMass(mCaO, 56, 40, 1);
 
   //initial mass of mixed metals that produced Machefer during combustion
-  double mInitialMix = mSiO2 + mAl + mFe + mCa + mOthers;
+  double mInitialMix = mSiO2 + mAl + mFe + mCa + mC + mCl;
 
   //now we can determine the relative proportions of metals
   //in the inert part of waste by normalizing each value
@@ -126,7 +126,8 @@ double Cm_Inert(double propSiO2, double propAl2O3, double propCaO, double propFe
   double inipropAl = mAl / mInitialMix;
   double inipropFe = mFe / mInitialMix;
   double inipropCa = mCa / mInitialMix;
-  double inipropOthers = mOthers / mInitialMix;
+  double inipropC = mC / mInitialMix;
+  double inipropCl = mCl / mInitialMix;
 
   //Now that we have relative proportions, we can finally approximate
   //its specific heat value by calculating the average of each heat capacity
@@ -137,10 +138,12 @@ double Cm_Inert(double propSiO2, double propAl2O3, double propCaO, double propFe
   double CmAl = 0.894;
   double CmFe = 0.412;
   double CmCa = 0.63;
-  double CmOthers = (0.710 + 0.48) / 2; //The average between the specific heat for Carbon, and that for Chlorine
+  double CmC = 0.710;
+  double CmCl = 0.48;
 
   //weighted average
-  double CmInert = (CmSiO2 * inipropSiO2) + (CmAl * inipropAl) + (CmFe * inipropFe) + (CmCa * inipropCa) + (CmOthers * inipropOthers);
+  double CmInert = (CmSiO2 * inipropSiO2) + (CmAl * inipropAl) + (CmFe * inipropFe);
+  CmInert += (CmCa * inipropCa) + (CmC * inipropC) + (CmCl * inipropCl);
 
   return CmInert;
 }
