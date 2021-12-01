5174df762f3d48009677ff758f58ad987630c475
Seif-Wessam <83871852+Seif-Wessam@users.noreply.github.com>
2021-12-01 15:02:18 +0100

    Part_2 exe and Part_3

--- Part3TRIDEL.py
+++ Part3TRIDEL.py
@@ -6,12 +6,21 @@ import math as ma
 """Part 1 CSV file creation""" 
 # 1st we need to import the Csv file into a table 
 
-#PowerTable = np.genfromtxt('PowerTable.csv', delimiter = ',', dtype = 'float64')
+PowerTable = np.genfromtxt('PowerTable.csv', delimiter = ',', dtype = 'float64') #[MW]
+PowerTable *= 1000000 #[W]
+
 wasteDayLst = np.genfromtxt('wasteDayLst.csv', delimiter = ',', dtype = 'float64') #[Kg]
 #wasteDayLst /= 1000 #[tonnes]
-#if 
+
 TridelValues = np.array([4942.84, 4909.77, 4959.43, 5041.34, 5876.92, 1396.43, 7550.33, 8926.98, 8242.48, 6819.23, 5744.39, 5439.18]) #[MWh / month]
 TridelValues = TridelValues / (30 * 24) #[MW]
+
+# rendre ces valeurs par jours: copier chaque valeur le nombre de jours
+# dans le mois correspondant
+
+def valueRepeater(List, repNumber):
+    for repetition in repNumber:
+        for value in range(list):
 
 
 
@@ -19,16 +28,18 @@ TridelValues = TridelValues / (30 * 24) #[MW]
 
 x = np.array([day + 1 for day in range(365)])
 
-#y = np.array(PowerTable)
 y = np.array(wasteDayLst)
+z = PowerTable
 
-plt.xlabel('Day')
-plt.ylabel('WasteDay [Kg]')
-plt.zlabel('')
-plt.title('Daily Waste Weight')
-plt.plot(x, y)
-
+#plt.title('Daily Waste Weight')
+plt.plot(x, y, label = 'Daily Waste weight [Kg]')
+plt.plot(x, z, label = 'Daily Power production [W]')
+plt.legend()
 plt.show()
 
-d = [i for i in range(13)]
-z = TridelValues
+d = [month for month in range(12)]
+e = TridelValues
+plt.plot(d, e, label = 'Tridel monthly Power production [MW]')
+plt.legend()
+plt.show()
+
