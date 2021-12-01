151a3ee879e64c73df0f2d61e08f25f23f819557
Seif-Wessam <83871852+Seif-Wessam@users.noreply.github.com>
2021-12-01 13:59:03 +0100

    Part 3

--- Part3TRIDEL.py
+++ Part3TRIDEL.py
@@ -7,8 +7,12 @@ import math as ma
 # 1st we need to import the Csv file into a table 
 
 #PowerTable = np.genfromtxt('PowerTable.csv', delimiter = ',', dtype = 'float64')
-wasteDayLst = np.genfromtxt('wasteDayLst.csv', delimiter = ',', dtype = 'float64')
+wasteDayLst = np.genfromtxt('wasteDayLst.csv', delimiter = ',', dtype = 'float64') #[Kg]
+#wasteDayLst /= 1000 #[tonnes]
 #if 
+TridelValues = np.array([4942.84, 4909.77, 4959.43, 5041.34, 5876.92, 1396.43, 7550.33, 8926.98, 8242.48, 6819.23, 5744.39, 5439.18]) #[MWh / month]
+TridelValues = TridelValues / (30 * 24) #[MW]
+
 
 
 """Part 2 energy output plot"""
@@ -17,6 +21,8 @@ x = np.array([day + 1 for day in range(365)])
 
 #y = np.array(PowerTable)
 y = np.array(wasteDayLst)
+z = TridelValues
 
 plt.plot(x, y)
+
 plt.show()
