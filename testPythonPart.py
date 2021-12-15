353d32e8f5608af07f689387ed27f5d5e7b2dd0c
Mikael <mikael.gonzalez@epfl.ch>
2021-12-15 15:29:35 +0100

    Korreturen

--- testPythonPart.py
+++ testPythonPart.py
@@ -145,9 +145,9 @@ import os
 while not os.path.exists("Part2TRIDEL.exe"):
 
    print("\nPart2TRIDEL.exe does not exist or isn't in the same folder as the program.\n")
-   print("Please, compile Part2TRIDEL.c or add the .exe file in the same folder.")
-   input("If Part2TRIDEL.exe is in the same folder as the program, press any key to continue...")
-
+   print("Please, compile Part2TRIDEL.c or add the .exe file in the same folder.\n")
+   input("Then, come back and press enter to continue...")
+   print("\n")
 os.startfile(r"Part2TRIDEL.exe")
 
 print("Creating a CSV file of calculated Power output...\n")
